e4967efddbf794b01dc3053a7912381cfb7bee41
Rafael Pambo <rafaelgeniokitoco@gmail.com>
2025-10-10 18:39:26 +0100

    xss aplicado

--- web/Onda_branca/src/app/components/home/hero/slides.ts
+++ web/Onda_branca/src/app/components/home/hero/slides.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-slides',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './slides.html',
   styleUrl: './slides.css'
 })
