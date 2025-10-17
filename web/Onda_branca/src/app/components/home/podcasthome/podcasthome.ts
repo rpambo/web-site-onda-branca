516a056c359b62b78ffed92aa85f39db294511b9
Rafael Pambo <rafaelgeniokitoco@gmail.com>
2025-10-17 13:27:33 +0100

    podcast section home  update

--- web/Onda_branca/src/app/components/home/podcasthome/podcasthome.ts
+++ web/Onda_branca/src/app/components/home/podcasthome/podcasthome.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-podcasthome',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './podcasthome.html',
   styleUrl: './podcasthome.css'
 })
